cfd81186c51d440b9ecbb022a06837006d0a8e00
Semen <semendubynin@gmail.com>
2020-05-20 16:50:05 +0300

    Initial Commit

---
+++ Memory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Дубынин Семён on 20.05.2020.
+//  Copyright © 2020 Дубынин Семён. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
